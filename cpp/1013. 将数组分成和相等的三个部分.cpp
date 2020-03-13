747c6e92a8a344ad9e14e6b0c4f7b46ebc0a5ac8
jackymxp <jacky@gmail.com>
2020-03-13 16:42:54 +0800

    1013 - 双指针

---
+++ cpp/1013. 将数组分成和相等的三个部分.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canThreePartsEqualSum(vector<int>& A) {
+        int len = A.size();
+        int sum = accumulate(A.begin(), A.end(), 0);
+        if(sum % 3 != 0)
+            return false;
+        int target = sum / 3;
+
+        //[0, left), [left, right), [right, len)
+        int left = 1, right = len-1;
+        int leftSum{A[0]}, rightSum(A[right]);
+        while(left < right)
+        {
+            //int leftSum =  accumulate(A.begin(), A.begin()+left, 0);
+            //int rightSum = accumulate(A.begin()+right, A.end(), 0);
+            if(leftSum ==  target &&
+               rightSum == target)
+               return true;
+            if(leftSum != target)
+                leftSum += A[left++];
+                
+            if(rightSum != target)
+                rightSum += A[--right];
+        }
+
+        return false;
+    }
+};
+
+int main(void)
+{
+    vector<int> arr = {0,2,1,-6,6,-7,9,1,2,0,1};
+    Solution s;
+    auto res = s.canThreePartsEqualSum(arr);
+    cout << res << endl;
+}
---
+++ cpp/1071. 字符串的最大公因子.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if(str1.empty() || str2.empty())
+            return "";
+        if(str1+str2 != str2+str1)
+            return "";
+        
+        
+        string tmp = str1.substr(0, gcd(str1.size(), str2.size()));
+        if(isgcd(str1, tmp) && isgcd(str2, tmp))
+            return tmp;
+        return "";
+        
+    }
+private:
+    int gcd(int a, int b)
+    {
+        //return __gcd(a, b);
+        assert(a != 0 || b != 0);
+        if(a == b)
+            return a;
+        else if(a < b)
+            return gcd(b, a);
+        else if(a > b)
+        {
+            if((a&0x1) && (b&0x1))
+                return gcd(b, a-b);
+            else if(!(a&0x1) && (b&0x1))
+                return gcd(a>>1, b);
+            else if((a&0x1) && !(b&0x1))
+                return gcd(a, b>>1);
+            else if(!(a&0x1) && !(b&0x1))
+                return gcd(a>>1, b>>1) << 1;
+        }
+    }
+    bool isgcd(const string& S, const string& T)
+    {
+        if(S.empty() || T.empty())
+            return false;
+        string tmp = T;
+        int len = S.size();
+        while(tmp.size() <= len)
+        {
+            if(tmp.size() < len)
+                tmp += T;
+            else if(tmp.size() == len)
+                return tmp == S;
+            else if(tmp.size() > len)
+                return false;
+        }
+        assert(1 == 2);
+        return false;
+    }
+};
+
+
+int main(void)
+{
+    Solution s;
+    string str1 = "ABCABC", str2 = "ABC";
+
+    string res = s.gcdOfStrings(str1, str2);
+    cout << "res = " << res << endl;
+}
