e84a7ff8128a31be80c935c3986c0e3d57b317c2
jackymxp <53531417+jackymxp@users.noreply.github.com>
2020-10-21 07:13:37 +0800

    Create 925. 长按键入.java

---
+++ cpp/925. 长按键入.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isLongPressedName(String name, String typed) {
+        if(name.length() == 0)
+            return typed.length() == 0;
+        int i = 0, j = 0;
+        while(j < typed.length()){
+            if(i < name.length() && name.charAt(i) == typed.charAt(j)){
+                i++;
+                j++;
+            }
+            else if(j > 0 && typed.charAt(j-1) == typed.charAt(j))
+                j++;
+            else
+                return false;
+        }
+        return i == name.length();
+    }
+}
